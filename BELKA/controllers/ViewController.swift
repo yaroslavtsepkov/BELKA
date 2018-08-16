ab31255ab88702cbd3bbd83b5c4c09b74d7e00c5
Yaroslav <yaroslav.tsepkov@gmail.com>
2018-08-16 11:19:15 +0400

    Основная верстка, остались константрейнты

--- BELKA/controllers/ViewController.swift
+++ BELKA/controllers/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func ButtonResetPassword(_ sender: Any) {
+    }
+    
+    @IBAction func buttonEnter(_ sender: Any) {
+    }
+    
+    @IBAction func buttonFacebookAuto(_ sender: Any) {
+    }
+    @IBAction func buttonVkAuto(_ sender: Any) {
+    }
+    @IBAction func buttonGoogleAuto(_ sender: Any) {
+    }
+    @IBAction func buttonYandexAuto(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +38,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
